7933bf50dd0d076f9f126206e58b1ec527cf704d
NikitaVovchok <154096755+NikitaVovchok@users.noreply.github.com>
2023-12-19 04:12:53 +0200

    Add files via upload

---
+++ Adapter pattern/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CryptoExchange
+{
+    // Абстрактний клас для представлення котирування криптовалюти
+    abstract class CurrencyQuotation
+    {
+        public abstract CurrencyQuotation Clone();
+        public abstract void Display();
+    }
+
+    // Конкретний клас для котирування криптовалюти 
+    class CryptoCurrencyQuotation : CurrencyQuotation
+    {
+        private string currency;
+        private double price;
+
+        public CryptoCurrencyQuotation(string currency, double price)
+        {
+            this.currency = currency;
+            this.price = price;
+        }
+
+        public override CurrencyQuotation Clone()
+        {
+            return new CryptoCurrencyQuotation(currency, price);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"{currency}: {price}");
+        }
+    }
+
+    // Клас для керування прототипами котирування криптовалюти
+    class CurrencyQuotationPrototypeManager
+    {
+        private Dictionary<string, CurrencyQuotation> prototypes = new Dictionary<string, CurrencyQuotation>();
+
+        public CurrencyQuotation this[string key]
+        {
+            get { return prototypes[key].Clone(); }
+            set { prototypes[key] = value; }
+        }
+    }
+
+    // Клас обміну криптовалюти, який використовує прототипи для створення котирування
+    class CryptoExchange
+    {
+        private List<CurrencyQuotation> quotations = new List<CurrencyQuotation>();
+
+        public void AddQuotation(CurrencyQuotation quotation)
+        {
+            quotations.Add(quotation);
+        }
+
+        public void DisplayQuotations()
+        {
+            foreach (var quotation in quotations)
+            {
+                quotation.Display();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Використання прототипів для створення котирування криптовалюти та додавання їх до обміну
+            CurrencyQuotationPrototypeManager prototypeManager = new CurrencyQuotationPrototypeManager();
+
+            prototypeManager["BTC"] = new CryptoCurrencyQuotation("BTC", 10000);
+            prototypeManager["ETH"] = new CryptoCurrencyQuotation("ETH", 2000);
+
+            // Використання Singleton для отримання єдиного екземпляра обміну
+            CryptoExchange exchange = new CryptoExchange();
+
+            // Додавання котирування криптовалюти до обміну, використовуючи клонування прототипів
+            exchange.AddQuotation(new CryptoCurrencyQuotation("BTC", 10000));
+            exchange.AddQuotation(new CryptoCurrencyQuotation("ETH", 2000));
+
+            // Виведення всіх котирувань криптовалюти в обміні
+            exchange.DisplayQuotations();
+        }
+    }
+}
---
+++ PaTTern Prototype/Program.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FunLibNamespace
+{
+    // Абстрактний клас для представлення криптобіржі
+    abstract class ExchangePrototype
+    {
+        public abstract ExchangePrototype Clone();
+        public abstract void Display();
+    }
+
+    // Конкретний клас для криптобіржі про біткоїн
+    class BitcoinExchange : ExchangePrototype
+    {
+        private string name;
+
+        public BitcoinExchange(string name)
+        {
+            this.name = name;
+        }
+
+        public override ExchangePrototype Clone()
+        {
+            return new BitcoinExchange(name);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Bitcoin Exchange: {name}");
+        }
+    }
+
+    // Конкретний клас для криптобіржі про етHEREUM
+    class EthereumExchange : ExchangePrototype
+    {
+        private string name;
+
+        public EthereumExchange(string name)
+        {
+            this.name = name;
+        }
+
+        public override ExchangePrototype Clone()
+        {
+            return new EthereumExchange(name);
+        }
+
+        public override void Display()
+        {
+            Console.WriteLine($"Ethereum Exchange: {name}");
+        }
+    }
+
+    // Клас для керування прототипами криптобірж
+    class ExchangePrototypeManager
+    {
+        private Dictionary<string, ExchangePrototype> prototypes = new Dictionary<string, ExchangePrototype>();
+
+        public ExchangePrototype this[string key]
+        {
+            get { return prototypes[key].Clone(); }
+            set { prototypes[key] = value; }
+        }
+    }
+
+    // Клас бібліотеки, який використовує прототипи для створення криптобірж
+    class FunLib
+    {
+        private List<ExchangePrototype> exchanges = new List<ExchangePrototype>();
+
+        public void AddExchange(ExchangePrototype exchange)
+        {
+            exchanges.Add(exchange);
+        }
+
+        public void DisplayExchanges()
+        {
+            foreach (var exchange in exchanges)
+            {
+                exchange.Display();
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main()
+        {
+            // Використання прототипів для створення криптобірж та додавання їх до бібліотеки
+            ExchangePrototypeManager prototypeManager = new ExchangePrototypeManager();
+
+            prototypeManager["Bitcoin"] = new BitcoinExchange("Coinbase");
+            prototypeManager["Ethereum"] = new EthereumExchange("Binance");
+
+            FunLib library = new FunLib();
+
+            // Додавання криптобірж до бібліотеки, використовуючи клонування прототипів
+            library.AddExchange(prototypeManager["Bitcoin"].Clone());
+            library.AddExchange(prototypeManager["Ethereum"].Clone());
+
+            // Виведення всіх криптобірж в бібліотеці
+            library.DisplayExchanges();
+        }
+    }
+}
